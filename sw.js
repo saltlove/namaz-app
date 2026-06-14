a0502431f12a5eb7f002d6e9184c47c96c8baa16
saltlove <shiest040275@gmail.com>
2026-06-15 01:06:06 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'namaz-v1';
+const OFFLINE_URLS = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE_URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('push', e => {
+  const data = e.data ? e.data.json() : { title: 'Время намаза', body: 'Наступило время молитвы' };
+  e.waitUntil(self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: './icon-192.png',
+    badge: './icon-192.png',
+    tag: 'prayer-time',
+    requireInteraction: false
+  }));
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('./'));
+});
